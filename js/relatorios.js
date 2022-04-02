f3364bb1588f65026ef892de126c7b61ab8f68f6
Ricardo Alves Roberto <ricardo.roberto@rdrtech.com.br>
2022-04-02 05:46:56 -0300

    nova url AWS-loja

--- js/relatorios.js
+++ js/relatorios.js
@@ -55,7 +55,7 @@ const totalResultadoMes = document.getElementById('total-resultado-mes');
 //const urlP = 'http://localhost:8080/' // local;
 // const urlP = 'http://54.165.200.215:8080/' // AWS;
 //const urlP = 'http://54.172.244.249:8080/' // AWS LOJA;
-const urlP = 'http://54.81.115.220/:8080/' // AWS LOJA;
+const urlP = 'http://54.81.115.220:8080/' // AWS LOJA;
 
 // variáveis globais
 let xhr = new XMLHttpRequest();
